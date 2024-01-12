393033d933902827b3d757c500e945326d94065a
neojarvis <techadmin@iamneo.ai>
2024-01-12 06:12:38 +0000

    Manual-save-commit-1/12/2024--11:42:36-AM

--- Project/src/test/java/pages/Demo.java
+++ Project/src/test/java/pages/Demo.java
@@ -2,7 +2,6 @@ package pages;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("qwerty");
-        System.out.println("submit project");
+        System.out.println("auto save");
     }
 }
